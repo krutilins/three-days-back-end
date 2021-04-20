fb6a4a4f0183cd5954c436979b6d66cefdfeb99e
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-04-20 15:26:25 +0300

    add vk authentication

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
+const session = require("express-session");
 const app = express();
 
-app.use(cors());
+app.use(cors(), session({ secret: "KGjkSVmsiQ2zwpjJ5M9d" }));
 
 app.get("/:token/:lon,:lat/realtime.json", (req, res) => {
   const apiKey = req.params.token;
@@ -19,9 +20,35 @@ app.get("/:token/:lon,:lat/realtime.json", (req, res) => {
     },
   })
     .then((response) => response.json())
-    .then((jsonResponse) => res.send(jsonResponse));
+    .then((jsonResponse) => res.send(jsonResponse))
+    .catch((error) => res.send(error));
 });
 
 app.listen(2020, () => {
   console.log("server is listening on port 2020");
+});
+
+/////////////////////////////////
+const VKONTAKTE_APP_ID = "7829476";
+const VKONTAKTE_APP_SECRET = "KGjkSVmsiQ2zwpjJ5M9d";
+
+app.get("/auth/vk", (req, res) => {
+  const url = `https://oauth.vk.com/access_token?client_id=${VKONTAKTE_APP_ID}&client_secret=${VKONTAKTE_APP_SECRET}&redirect_uri=${req.query.redirect_uri}&code=${req.query.code}`;
+
+  console.log(url);
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonResponse) => res.send(jsonResponse))
+    .catch((error) => res.send(error));
+});
+
+app.get("/vk/api/users/get", (req, res) => {
+  const accessToken = req.query.access_token;
+  const userId = req.query.user_id;
+  const url = `https://api.vk.com/method/users.get?access_token=${accessToken}&user_id=${user_id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonResponse) => res.send(jsonResponse))
+    .catch((error) => res.send(error));
 });
